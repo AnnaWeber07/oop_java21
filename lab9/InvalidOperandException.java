5d276b581dc45a5b264c25248f3bb6df3e4696f2
AnnaWeber07 <78998404+AnnaWeber07@users.noreply.github.com>
2021-11-29 11:11:12 +0200

    Add files via upload

---
+++ lab9/InvalidOperandException.java
@@ -0,0 +1,12 @@
+package annachiriciuc;
+
+public class InvalidOperandException extends Exception {
+    protected String log = "Operand is invalid!";
+
+    public InvalidOperandException() {
+    }
+
+    public void logPrint() {
+        System.out.println(this.log);
+    }
+}
